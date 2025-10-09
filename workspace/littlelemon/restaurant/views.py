ed4024ebe75d66ab445ead5ba27dfc9089accf24
ganesyahoo <ganes.mallik@yahoo.com>
2025-10-09 21:15:42 +0530

    added authorization

--- workspace/littlelemon/restaurant/views.py
+++ workspace/littlelemon/restaurant/views.py
@@ -1,8 +1,16 @@
 from rest_framework import generics, viewsets
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from .models import Booking, Menu
 from .serializers import BookingSerializer, MenuSerializer
 from django.shortcuts import render
+from rest_framework.response import Response
+
+@api_view()
+@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+def msg(request):
+    return Response({"message":"This view is protected"})
 
 # Create your views here.
 def index(request):
